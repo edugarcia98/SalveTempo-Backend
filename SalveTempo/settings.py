ece5a08d882dbce218116a0cc5e72377f2450900
edugarcia98 <edugarcia758@gmail.com>
2020-07-07 17:28:27 -0300

    Minor changes

--- SalveTempo/settings.py
+++ SalveTempo/settings.py
@@ -178,8 +178,8 @@ STATIC_URL = '/static/'
 
 # Email backend settings for Django
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = ''
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = 'dc^53vXZ\<'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
